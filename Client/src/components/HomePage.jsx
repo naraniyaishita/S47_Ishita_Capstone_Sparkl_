3f39b578e915c2f79130b07d8e95119b8c73088f
naraniyaishita <165628824+naraniyaishita@users.noreply.github.com>
2024-07-23 23:14:39 +0530

    Added spotify component

--- Client/src/components/HomePage.jsx
+++ Client/src/components/HomePage.jsx
@@ -1,16 +1,19 @@
-import React, {useContext} from 'react'
-import Navbar from './Navbar'
-import UserContext from './User/UserContext'
-import './Styles/Home.css'
-
+import React, { useContext } from "react";
+import Navbar from "./Navbar";
+import UserContext from "./User/UserContext";
+import "./Styles/Home.css";
+import Spotify from "./Pages/Spotify";
 function HomePage() {
-  const {username} = useContext(UserContext)
+  const { username } = useContext(UserContext);
   return (
     <div>
-      <Navbar/>
-      <h1 id='helloUser'>Hello, {username} </h1>
+      <Navbar />
+      <h1 id="helloUser">Hello, {username} </h1>
+      <div>
+        <Spotify />
+      </div>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
